ee23903a069c11b7a1e06c64089069fd8034bf98
jasper200207 <jasper12394@gmail.com>
2025-04-02 14:48:40 +0900

    add submodule for auth

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from logger import init_logview
 from const import SIZE, COLOR
 from actions import scan
 
+def test():
+    from iso15118.evcc.main import run
+    run()
+
 def _init_app():
+    global is_fullscreen
     root = Tk()
     root.geometry(f"{SIZE['SCREEN.W']}x{SIZE['SCREEN.H']}")
     root.configure(background=COLOR["BLACK1"])
+    root.attributes("-fullscreen", True)
+    root.bind("<f>", lambda event: root.attributes("-fullscreen", not root.attributes("-fullscreen")))
+    root.bind("<q>", lambda x : root.quit())
 
     logview = LogView(root)
     init_logview(logview)
@@ -27,6 +35,9 @@ def _init_app():
 
     scanningContainer = ScanningContainer(root)
     header.ScanButton.bind("<Button-1>", lambda x : scan(root, header, scanningContainer))
+    header.ScanButton.disable()
+    header.AuthButton.enable()
+    header.AuthButton.bind("<Button-1>", lambda x : test())
     scanningContainer.place(
         x=SIZE["DEFUALT_PADDING"],
         y=SIZE["DEFUALT_PADDING"]*2+SIZE["HEADER.H"],
