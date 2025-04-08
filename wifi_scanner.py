a8ed2d98fd1a0b5f14aab2fef222ea69883793e4
jasper200207 <jasper12394@gmail.com>
2025-04-08 18:05:48 +0900

    retry for scanning

--- wifi_scanner.py
+++ wifi_scanner.py
@@ -1,24 +1,38 @@
 import subprocess
 from logger import logging
+import environs, os
 
 def scanning_and_connect():
-    interface = "wlan0"
+    MAX_RETRY = 5
+    retry = 0
+    while retry < MAX_RETRY:
+        try:
+            env = environs.Env(eager=False)
+            env_path = os.getcwd() + "/.env"
+            env.read_env(path=env_path)
 
-    result = subprocess.run(f'bash ./evcc.sh {interface}', capture_output=True, text=True, shell=True)
-    logging(result.stdout)
-    result = result.stdout.split("\n")
-    add_network = result[-2].split(" ")[0]
-    
-    if not add_network:
-        logging("connect error")
-        return None, None, None, None, None
-    else:
-        bss = result[-2].split(" ")[1]
-        ssid = result[-2].split(" ")[2]
-        signal = float(result[-2].split(" ")[3])
-        freq = float(result[-2].split(" ")[4]) / 1000
-        vse = "".join(result[-2].split(" ")[5:])
-        return bss, ssid, signal, freq, vse
+            interface = env.str("NETWORK_INTERFACE", default="eth0")
+
+            result = subprocess.run(f'bash ./evcc.sh {interface}', capture_output=True, text=True, shell=True)
+            logging(result.stdout)
+            result = result.stdout.split("\n")
+            add_network = result[-2].split(" ")[0]
+            
+            if add_network:
+                bss = result[-2].split(" ")[1]
+                ssid = result[-2].split(" ")[2]
+                signal = float(result[-2].split(" ")[3])
+                freq = float(result[-2].split(" ")[4]) / 1000
+                vse = "".join(result[-2].split(" ")[5:])
+                return bss, ssid, signal, freq, vse
+            else:
+                raise Exception("Need Retry")
+        except Exception as e:
+            logging(e)
+            logging("retrying...")
+            retry += 1
+    logging("connect error")
+    return None, None, None, None, None
 
 if __name__ == '__main__':
     scanning_and_connect()
